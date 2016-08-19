cbadb468999eb2264adec4bc382a9ac71bcb0f8b
GregoireFavreau <trollnord@outlook.fr>
2016-08-19 18:22:15 +0200

    Update Main.php

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -6,4 +6,23 @@ use Core\View;
 use Core\Controller;
 use Helpers\Request as Request;
 
-class Main extends Controller 
+class Main extends Controller {
+  private $auth;
+  
+  public function __construct() {
+    parent::__construct();
+    $this->auth = new \Helpers\Auth\Auth();
+  }
+  
+  public function index() {
+    View::renderTemplate('header');
+    View::render('ajax_view');
+    View::renderTemplate('footer');
+  }
+  
+  public function secured() {
+    if ($this->auth->isLogged()) {
+      echo "all ok you are secured";
+    }
+  }
+}
